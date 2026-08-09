9302871355acbf8611356957cf41012286a9e4ed
BAVAJAN MASUNURI <bavajanmasunuri539@gmail.com>
2026-08-09 16:31:59 +0530

    format ALB Terraform module

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -39,10 +39,10 @@ resource "aws_lb" "this" {
   }
 }
 resource "aws_lb_target_group" "this" {
-  name     = "mcdp-${var.environment}-tg"
-  port     = var.target_port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "mcdp-${var.environment}-tg"
+  port        = var.target_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "instance"
   health_check {
     enabled             = true
