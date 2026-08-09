5cb451609b71abfcdb1b650e9636ebb60269add9
BAVAJAN MASUNURI <bavajanmasunuri539@gmail.com>
2026-08-09 16:17:26 +0530

    fix CI workflow and Dockerfiles

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -2,7 +2,6 @@ resource "aws_security_group" "alb" {
   name        = "mcdp-${var.environment}-alb-sg"
   description = "Security group for Application Load Balancer"
   vpc_id      = var.vpc_id
-
   ingress {
     description = "HTTP"
     from_port   = 80
@@ -10,7 +9,6 @@ resource "aws_security_group" "alb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     description = "HTTPS"
     from_port   = 443
@@ -18,43 +16,34 @@ resource "aws_security_group" "alb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "mcdp-${var.environment}-alb-sg"
   }
 }
-
 resource "aws_lb" "this" {
   name               = "mcdp-${var.environment}-alb"
   internal           = false
   load_balancer_type = "application"
-
   security_groups = [
     aws_security_group.alb.id
   ]
-
   subnets = var.public_subnet_ids
-
   tags = {
     Name = "mcdp-${var.environment}-alb"
   }
 }
-
 resource "aws_lb_target_group" "this" {
   name     = "mcdp-${var.environment}-tg"
   port     = var.target_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
   target_type = "instance"
-
   health_check {
     enabled             = true
     path                = "/"
@@ -65,31 +54,25 @@ resource "aws_lb_target_group" "this" {
     timeout             = 5
     interval            = 30
   }
-
   tags = {
     Name = "mcdp-${var.environment}-tg"
   }
 }
-
 resource "aws_lb_target_group_attachment" "this" {
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = var.target_instance_id
   port             = var.target_port
 }
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type = "forward"
-
     forward {
       target_group {
         arn = aws_lb_target_group.this.arn
       }
     }
-
   }
 }
